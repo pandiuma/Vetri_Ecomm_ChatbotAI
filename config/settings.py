d5f7994e74b676369ffef1054cf97e1c90b1cfa7
umamaheswari <pandiuma0306@gmail.com>
2026-09-16 17:04:19 +0530

    Fix static root for Vercel deployment

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv( "SECRET_KEY", "django-insecure-change-this-key" )
-DEBUG = os.getenv( "DEBUG", "True" ).lower() == "true"
+DEBUG = os.getenv( "DEBUG", "False" ).lower() == "true"
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
@@ -81,12 +81,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv("DATABASE_URL"):
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
@@ -124,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / 'store' / 'static',
 ]
