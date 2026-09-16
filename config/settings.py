6cead93006d69fb1773b184f7f906d187fea3055
umamaheswari <pandiuma0306@gmail.com>
2026-09-16 10:56:41 +0530

    Fix Vercel deployment host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.getenv( "DEBUG", "True" ).lower() == "true"
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    "vetri-ecomm-chatbot-ai.vercel.app",
+    ".vercel.app",
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
